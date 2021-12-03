d2281e675e61461e6b1f81854f7e578a6a774185
Duvan Rosero <duvanrosero1995@gmail.com>
2021-12-03 16:14:26 -0500

    creacion de interfaces basicas de cada entidad

---
+++ src/interfaces/database.ts
@@ -0,0 +1,26 @@
+export interface Usuario {
+    id_usuario: number;
+    nombres: string;
+    apellidos: string;
+    email: string;
+    celular: string;
+    direccion: string;
+}
+
+
+export interface Libro {
+    id_libro :  number;
+    titulo: string;
+    editorial:  string;
+    nombre_autor: string;
+    area: string;
+}
+
+export interface Prestamo {
+    prestamo_id: number;
+    libro_id:  number;
+    usuario_id : number;
+    fecha_prestamo: Date;
+    fecha_devolucion: Date;
+    devuelto: string;
+}
